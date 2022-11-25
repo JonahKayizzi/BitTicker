6453bdacdf78b23b644abb9b4bb27a47905f8ab2
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-11-25 15:40:39 +0300

    Display coins annd add search functionality to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,81 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { TwinSpin } from 'react-cssfx-loading';
+import { FaArrowCircleRight, FaCaretUp, FaCaretDown } from 'react-icons/fa';
+import { fetchCoins, searchCoins } from '../redux/bitcoins/bitcoins';
 
 const Home = () => {
   const fetchedCoins = useSelector((state) => state.coinSlice);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, []);
+  const { loading } = fetchedCoins;
+
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
+
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+    const { title } = inputText;
+    dispatch(searchCoins(title));
+  };
+
+  const displayCoins = fetchedCoins.searchedCoins.length !== 0
+    ? fetchedCoins.searchedCoins
+    : fetchedCoins.defaultCoins;
 
   return (
-    <section>
-      <ul className="coins-container">
-        {fetchedCoins.coins.map((coin) => (
-          <li key={coin.symbol} className="coin-wrapper">
-            <p className="coin-symbol">{coin.symbol}</p>
-            <div className="home-detail">
-              <small>{coin.bidPrice}</small>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <section className="coin-container">
+      <div className="operations">
+        <form className="form-container">
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Search other currency..."
+            value={inputText.title}
+            onChange={onChange}
+            name="title"
+          />
+        </form>
+      </div>
+      <p className="coins-heading">
+        {fetchedCoins.searchedCoins.length !== 0
+          ? `${fetchedCoins.searchedCoins.length} CURRENCIES FOUND`
+          : 'TOP 20 CURRENCIES'}
+      </p>
+      {!loading ? (
+        <ul className="coins-container">
+          {displayCoins.map((coin) => (
+            <li key={coin.id} className="coin-wrapper">
+              <Link to={coin.id}>
+                <div className={`${coin.id} coin-image`}>
+                  <FaArrowCircleRight />
+                </div>
+                <p className="coin-symbol">{coin.name.toUpperCase()}</p>
+                <div className="home-detail">
+                  <small>
+                    {coin.changePercent24Hr[0] === '-' ? (
+                      <FaCaretDown className="downHome" />
+                    ) : (
+                      <FaCaretUp className="upHome" />
+                    )}
+                  </small>
+                  <small>{`$ ${parseFloat(coin.priceUsd).toFixed(2)}`}</small>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <TwinSpin />
+      )}
     </section>
   );
 };
